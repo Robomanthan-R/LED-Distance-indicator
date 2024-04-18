b1c8ec2eb70e0838d4132ebe1d753a1336893ce4
Robomanthan-R <166908900+Robomanthan-R@users.noreply.github.com>
2024-04-18 21:41:52 +0530

    Update leddistance.ino

--- leddistance.ino
+++ leddistance.ino
@@ -1,12 +1,14 @@
-const int trigPin = 9; 
-const int echoPin = 10; 
-const int greenLed = 2; 
-const int yellowLed = 3; 
-const int redLed = 4; 
-const int greenDistance = 30; 
-const int yellowDistance = 20; 
-const int redDistance = 10; 
+const int trigPin = 9;
+const int echoPin = 10;
+const int greenLed = 2;
+const int yellowLed = 3;
+const int redLed = 4;
+const int greenDistance = 30;  // Distance in centimeters for green LED
+const int yellowDistance = 20; // Distance in centimeters for yellow LED
+const int redDistance = 10;    // Distance in centimeters for red LED
+
 long distance;
+
 void setup() {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
@@ -15,16 +17,20 @@ void setup() {
   pinMode(yellowLed, OUTPUT);
   pinMode(redLed, OUTPUT);
 }
+
 void loop() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  distance = pulseIn(echoPin, HIGH) / 58.2; 
+  
+  distance = pulseIn(echoPin, HIGH) / 58.2; // Calculate distance in centimeters
+  
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
+  
   if (distance > greenDistance) {
     digitalWrite(greenLed, HIGH);
     digitalWrite(yellowLed, LOW);
@@ -42,5 +48,7 @@ void loop() {
     digitalWrite(yellowLed, LOW);
     digitalWrite(redLed, LOW);
   }
+  
   delay(100);
 }
+
